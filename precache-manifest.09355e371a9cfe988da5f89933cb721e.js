d54df6e84e8c5dcb6ab382603660bac209b1f1a3
Jon Kim <kimhjona@gmail.com>
2019-06-26 18:43:18 -0700

    Updates

---
+++ precache-manifest.09355e371a9cfe988da5f89933cb721e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b82a2f7a8a26215ed55f694f3a47a36",
+    "url": "/hacker-news/index.html"
+  },
+  {
+    "revision": "9fcd586eb07a2ffb3cef",
+    "url": "/hacker-news/static/css/main.a7f7c08c.chunk.css"
+  },
+  {
+    "revision": "6e955a649549b0bddada",
+    "url": "/hacker-news/static/js/2.5c362ddb.chunk.js"
+  },
+  {
+    "revision": "9fcd586eb07a2ffb3cef",
+    "url": "/hacker-news/static/js/main.3f5a6bc3.chunk.js"
+  },
+  {
+    "revision": "f4cbfa7cef56548df7d2",
+    "url": "/hacker-news/static/js/runtime~main.393a1947.js"
+  }
+]);
